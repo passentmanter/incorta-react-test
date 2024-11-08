dee2764042f165254c11344b4aaeb42635146e4e
passent anter <passent.anter@ascertia.com>
2024-11-08 15:01:54 +0200

    add race list with tow types of view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
 
+// Pages
 import Home from "./pages/Home";
 import Navbar from "./components/navbar";
+import RaceList from "./pages/Races";
 
 import "./App.css";
+import "primeicons/primeicons.css";
 
 const App: React.FC = () => {
   return (
@@ -14,6 +17,7 @@ const App: React.FC = () => {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/season/:season" element={<RaceList />} />
           </Routes>
         </Router>
       </div>
