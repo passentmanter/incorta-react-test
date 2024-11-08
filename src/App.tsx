09c7dc934e6e5327db1e11785fb52c0e28c670a6
passent anter <passent.anter@ascertia.com>
2024-11-08 14:25:28 +0200

    add routing structure with homepage and navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AppProvider } from "./contexts/AppContext";
 
-function App() {
+import Home from "./pages/Home";
+import Navbar from "./components/navbar";
+
+import "./App.css";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+    </AppProvider>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import SeasonList from "../Season";
+
+// Assets
+import landingImage from "../../assets/landImg.jpeg";
+
+const Home: React.FC = () => {
+  return (
+    <div className="relative">
+      <img className="w-full h-screen" src={landingImage} alt="landingImage" />
+      {/* <SeasonList /> */}
+    </div>
+  );
+};
+
+export default Home;
